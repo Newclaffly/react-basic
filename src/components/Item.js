0f95571f30ae0d3393b836a3ff3a027973ef5492
Thatphon Chutinan <newclaffly40685@gmail.com>
2022-03-08 14:57:41 +0700

    add function symbol expense and income

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,9 +3,10 @@ import './Item.css'
 const Item = (props) => {
   const { title, amount } = props;
   const status = amount < 0 ? "expense" : "income";
+  const symbol = amount < 0 ? "-" : "+";
   return (
     <li className={status}>
-      {title} <span>{amount}</span>
+      {title} <span>{symbol}{Math.abs(amount)}</span>
     </li>
   );
 };
