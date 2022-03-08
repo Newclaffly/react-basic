9f90aa5cbee447f55d68786f0c6b1083983498e6
Thatphon Chutinan <newclaffly40685@gmail.com>
2022-03-08 14:10:10 +0700

    add usestate

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,26 +1,37 @@
+import { useState } from 'react';
 import './FormComponent.css'
 const FormComponent = () => {
   
+const [title,setTitle] = useState('')
+const [amount,setAmount] = useState(0)
+
   const inputTitle =(event)=>{
-      console.log(event.target.value)
+      setTitle(event.target.value)
   }
   const inputAmount =(event)=>{
-    console.log(event.target.value)
+    setAmount(event.target.value)
   }
   const saveItem =(event)=>{
     event.preventDefault()
     console.log("Record success")
+    const itemData = {
+      title:title,
+      amount:Number(amount)
+    }
+    console.log(itemData)
+    setTitle('')
+    setAmount(0)
   }
   return (
     <>
       <form onSubmit={saveItem}>
         <div className="form-control">
           <label>Name list</label>
-          <input type="text" placeholder="Please input list" onChange={inputTitle}></input>
+          <input type="text" placeholder="Please input list" onChange={inputTitle} value={title} ></input>
         </div>
         <div className="form-control">
           <labe>Amount</labe>
-          <input type="number" placeholder="(+ Income, - Expense)" onChange={inputAmount}></input>
+          <input type="number" placeholder="(+ Income, - Expense)" onChange={inputAmount} value={amount}></input>
         </div>
         <div>
           <button className="btn" type="submit" onClick={saveItem}>Add data</button>
