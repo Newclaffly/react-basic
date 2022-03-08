6114a206d433caed819e7b2cb7ff0146f694ca92
Thatphon Chutinan <newclaffly40685@gmail.com>
2022-03-08 14:53:23 +0700

    Add item style expense and income

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,18 @@
-import PropTypes from 'prop-types'; // ES6
-
+import PropTypes from "prop-types"; // ES6
+import './Item.css'
 const Item = (props) => {
   const { title, amount } = props;
+  const status = amount < 0 ? "expense" : "income";
   return (
-    <li>
+    <li className={status}>
       {title} <span>{amount}</span>
     </li>
   );
 };
 
-Item.prototype={
-  title:PropTypes.string.isRequired,
-  amount:PropTypes.number.isRequired
-}
+Item.prototype = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+};
 
 export default Item;
