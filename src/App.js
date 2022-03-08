d60923963b22cd593a2bebfed24ee7575768692b
Thatphon Chutinan <newclaffly40685@gmail.com>
2022-03-08 14:43:20 +0700

    componet child to parent app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import Transaction from "./components/Transaction";
-import './App.css'
-import FormComponent from "./components/FormComponent"
+import "./App.css";
+import FormComponent from "./components/FormComponent";
+import { useState } from "react";
 function App() {
+  // const initData = [
+  //   { id: 1, title: "Submarine", amount: 2000 },
+  //   { id: 2, title: "Bus", amount: 3000 },
+  //   { id: 3, title: "Car", amount: 4000 },
+  //   { id: 4, title: "Van", amount: 5000 },
+  //   { id: 5, title: "Air-plane", amount: 6000 },
+  //   { id: 6, title: "Boat", amount: 7000 },
+  // ];
+  const [items, setItem] = useState([]);
+  const onAddNewItem = (newItem) => {
+    setItem((prevItem) => {
+      return [newItem, ...prevItem];
+    });
+  };
   return (
-      <>
-      <div className="container"> 
-        <h1>
-        Income and Expenses basic-app
-        </h1>
-        <FormComponent/>
-        <Transaction />
+    <>
+      <div className="container">
+        <h1>Income and Expenses basic-app</h1>
+        <FormComponent onAddItem={onAddNewItem} />
+        <Transaction items={items} />
       </div>
-      </>
+    </>
   );
 }
 
--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,44 +1,58 @@
-import { useState } from 'react';
-import './FormComponent.css'
-const FormComponent = () => {
-  
-const [title,setTitle] = useState('')
-const [amount,setAmount] = useState(0)
+import { useState } from "react";
+import "./FormComponent.css";
+import { v4 as uuidv4 } from "uuid";
 
-  const inputTitle =(event)=>{
-      setTitle(event.target.value)
-  }
-  const inputAmount =(event)=>{
-    setAmount(event.target.value)
-  }
-  const saveItem =(event)=>{
-    event.preventDefault()
-    console.log("Record success")
+const FormComponent = (props) => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const inputTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const inputAmount = (event) => {
+    setAmount(event.target.value);
+  };
+  const saveItem = (event) => {
+    event.preventDefault();
+    console.log("Record success");
     const itemData = {
-      title:title,
-      amount:Number(amount)
-    }
-    console.log(itemData)
-    setTitle('')
-    setAmount(0)
-  }
+      id: uuidv4(),
+      title: title,
+      amount: Number(amount),
+    };
+    props.onAddItem(itemData)
+    setTitle("");
+    setAmount(0);
+  };
   return (
     <>
       <form onSubmit={saveItem}>
         <div className="form-control">
           <label>Name list</label>
-          <input type="text" placeholder="Please input list" onChange={inputTitle} value={title} ></input>
+          <input
+            type="text"
+            placeholder="Please input list"
+            onChange={inputTitle}
+            value={title}
+          ></input>
         </div>
         <div className="form-control">
           <labe>Amount</labe>
-          <input type="number" placeholder="(+ Income, - Expense)" onChange={inputAmount} value={amount}></input>
+          <input
+            type="number"
+            placeholder="(+ Income, - Expense)"
+            onChange={inputAmount}
+            value={amount}
+          ></input>
         </div>
         <div>
-          <button className="btn" type="submit" onClick={saveItem}>Add data</button>
+          <button className="btn" type="submit" onClick={saveItem}>
+            Add data
+          </button>
         </div>
       </form>
     </>
   );
 };
 
-export default FormComponent
+export default FormComponent;
